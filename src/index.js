563e8df27aee63b35bd15f30adb46357c150960c
amitesh <amitesh.chapter247@gmail.com>
2019-01-12 17:25:03 +0530

    Burger Builder

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Task from  './Task2/Task';
+import BurgerBuilder from './Container/BurgerBuilder'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Task />, document.getElementById('root'));
+ReactDOM.render(<BurgerBuilder />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
